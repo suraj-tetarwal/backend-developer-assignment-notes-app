720487fa8d2fc20a6074327a010ae69cbffcb6ef
Suraj Tetarwal <lord.skt210503@gmail.com>
2026-02-21 12:37:51 +0530

    add postgres production config

--- backend/config/config.js
+++ backend/config/config.js
@@ -9,10 +9,7 @@ module.exports = {
     dialect: process.env.DB_DIALECT || "mysql",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT || "mysql",
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
   },
 };
