5fe95bc65855dedc2688a1537e9c1bd94a971567
Suraj Tetarwal <lord.skt210503@gmail.com>
2026-02-21 12:46:03 +0530

    change database config for submission

--- backend/config/config.js
+++ backend/config/config.js
@@ -9,7 +9,10 @@ module.exports = {
     dialect: process.env.DB_DIALECT || "mysql",
   },
   production: {
-    use_env_variable: "DATABASE_URL",
-    dialect: "postgres",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || "mysql",
   },
 };
